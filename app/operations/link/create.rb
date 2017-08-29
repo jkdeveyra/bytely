d1174e946c2d62e951759f36c5a59eb17deddb4f
JK de Veyra <jkdeveyra@gmail.com>
2017-08-30 01:46:55 +0800

    Extract unique code generator

--- app/operations/link/create.rb
+++ app/operations/link/create.rb
@@ -1,20 +1,12 @@
 # Creates or returns an existing Link object which represents the shorten link
-# Accepted hash params:
-#   { link:  {
-#       title,
-#       url
-#   }}
+# Accepted hash params: link: { title, url }
 class Link::Create < Operation
-  DEFAULT_MIN_LENGTH = 3.freeze
-  LENGTH_RANGE = 1.freeze
-  MIN_LEN_KEY = 'LINK_CODE_MIN_LENGTH'.freeze
-
   def run(params)
     set_params(params)
     begin
       link = Link.where(url: link_params[:url]).first_or_create(
         title: link_params[:title],
-        code: rand_code
+        code: Link::GenerateCode.run
       )
       success = true
     rescue Exception => e # e is used for debugging
@@ -36,30 +28,6 @@ class Link::Create < Operation
     else
       @params = ActionController::Parameters.new
     end
-  end
-
-  # Guarantees to return a unique random code.
-  # Increments code length when collides more than 2 times
-  def rand_code
-    loop_count = 0
-    loop do
-      code = RandomCode.generate(min: min_len, max: min_len + LENGTH_RANGE)
-      if Link.where(code: code).exists?
-        loop_count++
-        inc_min_len if loop_count > 1
-      else
-        return code
-      end
-    end
-  end
-
-  def min_len
-    @min_len ||= AppProp.where(key: MIN_LEN_KEY).first&.value&.to_i || DEFAULT_MIN_LENGTH
-  end
-
-  def inc_min_len
-    @min_len = min_len + 1
-    AppProp.where(key: MIN_LEN_KEY).first_or_create(value: @min_len)
   end
 
   def link_params
---
+++ app/operations/link/generate_code.rb
@@ -0,0 +1,33 @@
+# Service for generating a unique shorten code
+# used to create Link record
+# Increments code length when collides more than twice
+class Link::GenerateCode < Operation
+  DEFAULT_MIN_LENGTH = 1.freeze
+  LENGTH_RANGE = 1.freeze
+  MIN_LEN_KEY = 'LINK_CODE_MIN_LENGTH'.freeze
+
+  def run
+    loop_count = 0
+    loop do
+      code = RandomCode.generate(min: min_len, max: min_len + LENGTH_RANGE)
+      if Link.where(code: code).exists?
+        puts "collide code: #{code} loop_count: #{loop_count}"
+        loop_count += 1
+        inc_min_len if loop_count > 1
+      else
+        return code
+      end
+    end
+  end
+
+  def min_len
+    @min_len ||= AppProp.where(key: MIN_LEN_KEY).first&.value&.to_i || DEFAULT_MIN_LENGTH
+  end
+
+  def inc_min_len
+    @min_len = min_len + 1
+    prop = AppProp.where(key: MIN_LEN_KEY).first_or_initialize
+    prop.value = @min_len
+    prop.save
+  end
+end
