1276d6bbb4e16c731fcd9765d0d69855c0ad31ea
jkdeveyra <jkdeveyra@gmail.com>
2017-09-06 18:41:41 +0800

    Add feature spec for shortening invalid links

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -9,10 +9,10 @@ feature 'Create link', js: true do
   end
 
   scenario 'Shorten link in the homepage' do
-    visit '/'
+    url = Faker::Internet.url
 
-    fill_in 'link[url]', with: Faker::Internet.url
-    find('input[type="submit"]').click
+    visit '/'
+    fill_in_and_submit_url url
 
     expect(Link.count).to equal 1
     expect(page).to have_content "#{server_host_and_port}/#{Link.first.code}"
@@ -22,10 +22,30 @@ feature 'Create link', js: true do
     link = create(:link, url: 'http://www.google.com')
 
     visit '/'
-    fill_in 'link[url]', with: link.url
-    find('input[type="submit"]').click
+    fill_in_and_submit_url link.url
 
     expect(Link.count).to equal 1
     expect(page).to have_content "#{server_host_and_port}/#{link.code}"
   end
+
+  scenario 'Invalid URL show validation error' do
+    invalid_urls = %w'
+      abcd
+      ftp://google.com
+      httpp:google.com
+      http://:3000
+      john@gmail.com'
+
+    visit '/'
+    invalid_urls.each do |url|
+      fill_in_and_submit_url url
+      expect(page).to have_content 'Link must be a valid web URL.'
+      page.evaluate_script 'window.location.reload()'
+    end
+  end
+end
+
+def fill_in_and_submit_url(url)
+  fill_in 'link[url]', with: url
+  find('input[type="submit"]').click
 end
